c2e6be868289e926835b72f5fe8598fae49df522
avorhaus <68480789+avorhaus@users.noreply.github.com>
2021-04-13 12:38:34 -0400

    Create streamlit_app.py
    added the base streamlit.py

---
+++ streamlit_app.py
@@ -0,0 +1,14 @@
+  
+# streamlist_app.py
+# Streamlit application.
+#
+# Abby Vorhaus, Kyle Dotterrer
+
+import numpy as np
+import pandas as pd
+import altair as alt
+import streamlit as st
+from vega_datasets import data
+
+# The relative path to the directory in which data is stored
+DATA_PATH = "data/"
