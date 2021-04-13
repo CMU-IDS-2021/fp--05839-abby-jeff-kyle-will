783d459054966dc0712768abe1cf78aa1298d779
avorhaus <68480789+avorhaus@users.noreply.github.com>
2021-04-13 14:03:29 -0400

    Update streamlit_app.py
    Added some basic structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,94 @@ COLOR_SCHEME_BLUE = "#90c1dc"
 st.set_page_config(layout="wide")
 
 st.header("Here We Go")
+
+
+# -----------------------------------------------------------------------------
+# Introduction
+# -----------------------------------------------------------------------------
+def render_introduction_content():
+    """
+    Render the introduction content.
+    """
+
+    '''
+    # Machine Super Intelligence
+    
+    ### The real reason we haven't heard from the aliens
+    '''
+    
+    st.sidebar.header("Digging Deeper")
+    st.sidebar.write(
+        "We are only grazing the surface with our main graphics, " +
+        "but you can keep exploring! Below you will find options " + 
+        "for each section that will allow you to explore the data.")
+
+# -----------------------------------------------------------------------------
+# Conclusion
+# -----------------------------------------------------------------------------
+def render_conclusion_content():
+    """
+    Render the conclusion content.
+    """
+
+    '''
+    # Conclusion
+    
+    This concludes our exploration of the machine super intelligence! Don't worry, the machines already won.
+    '''
+# -----------------------------------------------------------------------------
+# Chapter: Terminator
+# -----------------------------------------------------------------------------
+
+def render_chapter_one():
+    """
+    Render chapter one.
+    """
+
+    '''
+    ---
+    # Terminator 
+    
+    Hasta la vista baby
+    '''
+
+# -----------------------------------------------------------------------------
+# Chapter: HAL
+# -----------------------------------------------------------------------------
+
+def render_chapter_two():
+    """
+    Render chapter two.
+    """
+
+    '''
+    ---
+    # HAL 3000 
+    
+    I know that you and Frank were planning to disconnect me, and I'm afraid that's something I cannot allow to happen.
+    '''
+
+
+# -----------------------------------------------------------------------------
+# Main
+# -----------------------------------------------------------------------------
+
+def main():
+    render_introduction_content()
+
+    # Chapter 1: TODO
+    render_chapter_one()
+
+    # Chapter 2: TODO
+    render_chapter_two()
+
+    # ADD MORE CHAPTERS AS NEEDED
+
+    render_conclusion_content()
+
+# -----------------------------------------------------------------------------
+# Application Entry Point
+# -----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    main()
