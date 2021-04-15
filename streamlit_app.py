cdc8632bc44d0f2235ef81f78cf74cc2d5f71460
Kyle Dotterrer <kdotterrer@gmail.com>
2021-04-15 10:54:03 -0400

    basic layout for narrative

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@
 #
 # Abby Vorhaus, Jeff Moore, Kyle Dotterrer, Will Borom
 
+import psw
 import numpy as np
 import pandas as pd
 import altair as alt
@@ -11,7 +12,6 @@ import streamlit as st
 import matplotlib.pyplot as plt
 from streamlit_timeline import timeline
 import vega_datasets 
-import psw
 
 
 # The relative path to the directory in which data is stored
@@ -40,13 +40,12 @@ def render_introduction_content():
     """
     Render the introduction content.
     """
-    coll, colm, colr = st.beta_columns([1,4,1])
-    colm.title('Machine Superintelligence')
 
     '''
+    # Machine Intelligence: Risks and Opportunities
+
     ---
     TODO: Compelling introduction content here.
-
     '''
     
     st.sidebar.header("Digging Deeper")
@@ -78,160 +77,120 @@ def render_history_chapter():
     timeline(data, height=500)
 
 # -----------------------------------------------------------------------------
-# Chapter: Terminator
+# Chapter: Paradigm
 # -----------------------------------------------------------------------------
 
-def render_terminator_chapter():
+def render_paradigm_chapter():
     """
-    Render chapter one.
+    Render the paradigm shift chapter.
     """
 
     '''
-    ---
-    # AI Super Intelligence and Public Policy
-    
-    What do the World Powers think?
-    
+    # Machine Intelligence: A Paradigm Shift
+
+    TODO: Content here.
     '''
-    #read in data as csv
-    COUNTRY_VOTES =  pd.read_csv("data/CountryVotesonAI.csv")
-    #combine totally agree/tend agree to one agree column for user clarity
-    COUNTRY_VOTES["Agree"] = COUNTRY_VOTES["Tend_Agree"] + COUNTRY_VOTES["Totally_Agree"]
-    #combine totally disagree/tend disagree to one disagree column for user clarity
-    COUNTRY_VOTES["Disagree"] = COUNTRY_VOTES["Tend_Disagree"] + COUNTRY_VOTES["Totally_Disagree"]
-    COUNTRY_VOTES = COUNTRY_VOTES.rename(columns={"Totally_Agree": "Totally Agree", "Tend_Agree": "Tend Agree", "Tend_Disagree": "Tend Disagree", "Totally_Disagree": "Totally Disagree"})
-
-    country_selector = alt.selection_multi(fields=['Country'], init=[{'Country':'Hungary'}])
-    
-    #transform fold is conducted so that we can utilize the stacked bar approach
-    #and analyze the huge discrepanacy between what world powers think
-    '''
-    ## World Power Believe on AI Safety and Regulation
-    '''
-
-    all_country_data = alt.Chart(COUNTRY_VOTES).mark_bar().encode(
-        x=alt.X('Agree:Q', title="Percent Representatives that Want AI Regulation", scale=alt.Scale(domain=[0, 100])),
-        y=alt.Y('Country:N', title="Country"), 
-        color=alt.condition(country_selector, alt.value('#714141'), alt.value('#d9d9d9'))
-    ).add_selection(
-        country_selector
-    ).interactive()
-
-
-    by_country2 = alt.Chart(COUNTRY_VOTES).transform_fold(
-        ['Agree', 'Disagree'],
-        as_=['Sentiment', 'Agree/Disagree']
-    ).mark_bar().encode(
-        x=alt.X('Country:N', title='Country'),
-        y=alt.Y('Agree/Disagree:Q', title='Is AI Regulation Needed?', scale=alt.Scale(domain=[0, 100])),
-        tooltip=[alt.Tooltip("Country:N", title='Country')], 
-        color=alt.Color('Sentiment:N',
-            scale = alt.Scale(domain=['Agree', 'Disagree'], range=['#714141', '#d9d9de'])
-        )
-    ).transform_filter(
-        country_selector
-    ).interactive()
-
-    joint_chart = all_country_data | by_country2
-
-
-
-    st.write(joint_chart)
-    st.write("From the chart above we can see that nearly every major world power agrees" +
-    " that AI imposes enough risk that it should be regulated. There is not one outlier nation " +
-    "that does not think that AI and Robotics are begning to play in increasingly " +
-    "dangerous or volatile role.")
-
 
 # -----------------------------------------------------------------------------
-# Chapter: HAL
+# Chapter: Perceptions
 # -----------------------------------------------------------------------------
 
-def render_hal_chapter():
+def render_perceptions_chapter():
     """
     Render chapter two.
     """
+
     '''
     ---
-    # HAL 3000 
+    # Perceptions versus Reality 
 
-    I know that you and Frank were planning to disconnect me, and I'm afraid that's something I cannot allow to happen.
-
+    TODO: Content here.
     '''
+    
     '''
-    ## What is the view of Artificial Intelligence in the media?  
+    ## What is the view of Artificial Intelligence in the media? 
+     
     The following word cloud expresses some of the sentiment that can be seen in recent articles about AI.
     '''
+
     articleData = psw.getData("data/articles.json")
     articleText = psw.buildWordCloudText(articleData)
     wc = psw.getWordCloud(articleText)
     st.write(wc)
+    
     '''
     The table below shows a document level sentiment analysis of each of the articles that were pulled from Google today.  
     As you can see there is mostly positive sentiment associated with AI from the articles.
     '''
+    
     finalSent = pd.read_csv("data/sentiment.csv")
     sentChart = psw.buildChart(finalSent)
     st.write(sentChart)
 
-
-
 # -----------------------------------------------------------------------------
-# Chapter: What Would You Choose
+# Chapter: Prospects
 # -----------------------------------------------------------------------------
-odds_dict = {"1 in 2": 2, 
-            "1 in 3" : 3, 
-            "1 in 4" : 4, 
-            "1 in 5" : 5, 
-            "1 in 10" : 10}
 
-age_dict = {"18 to 29":"18 - 29",
-            "30 to 44": "30 - 44", 
-            "45 to 59": "45 - 59", 
-            "60+" : "60"}
+odds_dict = {
+    "1 in 2": 2, 
+    "1 in 3" : 3, 
+    "1 in 4" : 4, 
+    "1 in 5" : 5, 
+    "1 in 10" : 10
+}
 
-gender_dict = {"Female": "female",
-                "Male" : "male"}
+age_dict = {
+    "18 to 29":"18 - 29",
+    "30 to 44": "30 - 44", 
+    "45 to 59": "45 - 59", 
+    "60+" : "60"
+}
+
+gender_dict = {
+    "Female": "female",
+    "Male" : "male"
+}
 
 def user_selection(df):
-    # center the columns
+    # Center the columns
     col1, col2, col3 = st.beta_columns([1,3,1])
 
-    # set up the radio buttons
+    # Set up the radio buttons
     age_range = col2.radio("Choose your age bracket from the provided ranges.", ("-", "18 to 29", "30 to 44", "45 to 59", "60+"))   
     if age_range != "-":
         gender_at_birth = col2.radio("Choose your sex.", ("-", "Female", "Male"))
         if gender_at_birth != "-":
-            odds = col2.radio("Choose the Odds. Remember, this is the chance that humanity will face complete destruction.", 
+            odds = col2.radio(
+                "Choose the Odds. Remember, this is the chance that humanity will face complete destruction.", 
                 ("-", "1 in 2", "1 in 3", "1 in 4", "1 in 5", "1 in 10")) 
             if odds != "-":
-                #given the parameters get the specified percentages and graph for the user
+                # Given the parameters get the specified percentages and graph for the user
                 yes_or_no = col2.radio("Given the odds, should we as humanity continue to pursue this advancement?", ("-", "Yes", "No"))
 
-                # retrieve the specified row of data
-                selection = df.loc[(df['Gender at Birth'] == gender_at_birth) & (df['Age Range'] == age_dict.get(age_range))
-                            & (df['Odds'] == odds_dict.get(odds))]
+                # Retrieve the specified row of data
+                selection = df.loc[(df["Gender at Birth"] == gender_at_birth) & (df["Age Range"] == age_dict.get(age_range))
+                            & (df["Odds"] == odds_dict.get(odds))]
                 selection = selection.reset_index()
-                graph_select = selection.melt(id_vars=['Age Range', 'Gender at Birth'], value_vars=['No','Yes'],
-                                var_name = 'Decision', value_name = 'Percent')
-                #return specific message to user
+                graph_select = selection.melt(id_vars=["Age Range", "Gender at Birth"], value_vars=["No", "Yes"],
+                                var_name = "Decision", value_name = "Percent")
+                # Return specific message to user
                 if yes_or_no != "-":
                     if yes_or_no == "Yes":
-                        y_percent = ((selection.at[0, 'Yes'])*100).round(1)
+                        y_percent = ((selection.at[0, "Yes"])*100).round(1)
                         st.write("You agreed with " + str(y_percent) + " percent of individuals who are " + gender_dict.get(gender_at_birth) + 
                         " and are in the age bracket of " + age_range + " that we should press on despite the risks")
                     elif yes_or_no == "No":
-                        n_percent = ((selection.at[0, 'No'])*100).round(1)
+                        n_percent = ((selection.at[0, "No"])*100).round(1)
                         st.write("You agreed with " + str(n_percent)  + " percent of individuals who are " + gender_dict.get(gender_at_birth) + 
                         " and are in the age bracket of " + age_range +  " that jeopardizing humanity to that degree is simply not worth it")
                     
-                    #Build and return Bar chart
+                    # Build and return Bar chart
                     graph_title = "Decision Point: Odds " + odds
                     basic_bar = alt.Chart(graph_select).mark_bar().encode(
-                            x=alt.X('Decision'),
-                            y=alt.Y('Percent:Q', scale=alt.Scale(domain=(0, 1)), axis=alt.Axis(format='%', title='Percentage')),
-                            color=alt.Color('Decision:N',scale=alt.Scale(scheme="redyellowblue")),
-                            tooltip=[alt.Tooltip('Age Range:N'), alt.Tooltip('Gender at Birth:N'), alt.Tooltip('Percent:Q', format='.2%')]
+                            x=alt.X("Decision"),
+                            y=alt.Y("Percent:Q", scale=alt.Scale(domain=(0, 1)), axis=alt.Axis(format="%", title="Percentage")),
+                            color=alt.Color("Decision:N",scale=alt.Scale(scheme="redyellowblue")),
+                            tooltip=[alt.Tooltip("Age Range:N"), alt.Tooltip("Gender at Birth:N"), alt.Tooltip("Percent:Q", format=".2%")]
                         ).properties(
                             title= graph_title
                         ).properties(
@@ -252,18 +211,19 @@ def regions_viz(oddsDf, odds):
     subtitle = odds + " odds of total destruction of humanity - note percentages are by region"
 
     uschart = alt.Chart(states).mark_geoshape().encode(
-            tooltip=['US Region:N', 'State:N', alt.Tooltip('No:Q', format='.2%'), alt.Tooltip('Yes:Q', format='.2%')],
-            color=alt.Color('Percentage That Said No:Q', scale=alt.Scale(scheme="redyellowblue", reverse=True))
+            tooltip=["US Region:N", "State:N", alt.Tooltip("No:Q", format=".2%"), alt.Tooltip("Yes:Q", format=".2%")],
+            color=alt.Color("Percentage That Said No:Q", scale=alt.Scale(scheme="redyellowblue", reverse=True))
         ).transform_lookup(
-            lookup='id',
-            from_=alt.LookupData(oddsDf, 'id', ['US Region', 'State', 'No', 'Yes', 'Percentage That Said No']),
+            lookup="id",
+            from_=alt.LookupData(oddsDf, "id", ["US Region", "State", "No", "Yes", "Percentage That Said No"]),
         ).properties(
             width= DEFAULT_WIDTH,
             height= DEFAULT_HEIGHT,
         ).project(
-            type='albersUsa'
+            type="albersUsa"
         ).properties(
-            title= {"text": ["Should we Continue to Pursue Advancement?"], 
+            title= {
+            "text": ["Should we Continue to Pursue Advancement?"], 
             "subtitle": subtitle,
            }
         )
@@ -274,16 +234,21 @@ def regions_viz(oddsDf, odds):
     )
     return uschart
 
-
-def render_user_choice():
+def render_prospects_chapter():
     """
     Render chapter four user choice.
     """
+    
     '''
     ---
+    # Prospects for Machine Intelligence
+
+    TODO: Content here.
     '''
-    coll, colm = st.beta_columns([2,5.5])
-    colm.title('What Do You Choose?')
+
+    # Create centered title content
+    # coll, colm = st.beta_columns([2,5.5])
+    # colm.title('What Do You Choose?')
    
     '''
     
@@ -302,12 +267,10 @@ def render_user_choice():
 
     Next, select one of the odds. The odds represent the chance that humanity will be destroyed. For example, 1 in 3 implies that one out
     of every three times, humanity will be completely and totally annihilated by the advancement we achieved. However, the other two times the
-    result will be a world with no famine, disease, poverty, suffering, or death - in short a utopia. You decide whether we should continue down the
-    path or not.
+    result will be a world with no famine, disease, poverty, suffering, or death - in short a utopia. You decide whether we should continue down the path or not.
     '''
-    #
     
-    #Set up the Radio Button Theme
+    # Set up the radio button theme
     st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
     st.write('<style>div.row-widget.stRadio > div[role="radiogroup"] > label[data-baseweb="radio"] > div:first-child {background-color:'
         + COLOR_SCHEME_BLUE + '};</style>', unsafe_allow_html=True)
@@ -318,11 +281,11 @@ def render_user_choice():
             } 
         </style> """, unsafe_allow_html=True)
 
-    #let the use choose their fate
+    # Let the use choose their fate
     selectDf = pd.read_csv(DATA_PATH + "grouped-Ending-Humanity.csv")
     user_selection(selectDf)
 
-    #SideBar
+    # Sidebar
     st.sidebar.header("What Do You Choose?")
     st.sidebar.write(
         "Explore more about how the United States public responded to the survey. You can look at region maps as well as " 
@@ -334,8 +297,9 @@ def render_user_choice():
     region5 = pd.read_csv(DATA_PATH + "regions5-Ending-Humanity.csv")
     region10 = pd.read_csv(DATA_PATH + "regions10-Ending-Humanity.csv")
 
-    region_map_select = st.sidebar.selectbox("Select on of the odds to see how different regions across the United States answered the survey question.",
-                        ("-", "1 in 2", "1 in 3", "1 in 4", "1 in 5", "1 in 10"))
+    region_map_select = st.sidebar.selectbox(
+        "Select on of the odds to see how different regions across the United States answered the survey question.",
+        ("-", "1 in 2", "1 in 3", "1 in 4", "1 in 5", "1 in 10"))
     if region_map_select != "-":
         if region_map_select == "1 in 2":
             st.write(regions_viz(region2, region_map_select))
@@ -347,8 +311,81 @@ def render_user_choice():
             st.write(regions_viz(region5, region_map_select))
         else:
             st.write(regions_viz(region10, region_map_select))
-    
 
+# -----------------------------------------------------------------------------
+# Chapter: Responses and Future Directions
+# -----------------------------------------------------------------------------
+
+def render_responses_chapter():
+    """
+    Render the responses chapter.
+    """
+
+    '''
+    ---
+    # Responses to Machine Intelligence
+    
+    TODO: Content here.
+    '''
+
+    '''
+    ### What do the world powers think?
+    '''
+
+    # Read in data as csv
+    COUNTRY_VOTES =  pd.read_csv("data/CountryVotesonAI.csv")
+    
+    # Combine totally agree/tend agree to one agree column for user clarity
+    COUNTRY_VOTES["Agree"] = COUNTRY_VOTES["Tend_Agree"] + COUNTRY_VOTES["Totally_Agree"]
+    
+    # Combine totally disagree/tend disagree to one disagree column for user clarity
+    COUNTRY_VOTES["Disagree"] = COUNTRY_VOTES["Tend_Disagree"] + COUNTRY_VOTES["Totally_Disagree"]
+    COUNTRY_VOTES = COUNTRY_VOTES.rename(
+        columns={
+            "Totally_Agree": "Totally Agree",
+            "Tend_Agree": "Tend Agree",
+            "Tend_Disagree": "Tend Disagree",
+            "Totally_Disagree": "Totally Disagree"
+            })
+
+    country_selector = alt.selection_multi(fields=["Country"], init=[{"Country":"Hungary"}])
+    
+    # Transform fold is conducted so that we can utilize the stacked bar approach
+    # and analyze the huge discrepanacy between what world powers think
+    
+    '''
+    ## World Power Believe on AI Safety and Regulation
+    '''
+
+    all_country_data = alt.Chart(COUNTRY_VOTES).mark_bar().encode(
+        x=alt.X("Agree:Q", title="Percent Representatives that Want AI Regulation", scale=alt.Scale(domain=[0, 100])),
+        y=alt.Y("Country:N", title="Country"), 
+        color=alt.condition(country_selector, alt.value("#714141"), alt.value("#d9d9d9"))
+    ).add_selection(
+        country_selector
+    ).interactive()
+
+
+    by_country2 = alt.Chart(COUNTRY_VOTES).transform_fold(
+        ["Agree", "Disagree"],
+        as_=["Sentiment", "Agree/Disagree"]
+    ).mark_bar().encode(
+        x=alt.X("Country:N", title="Country"),
+        y=alt.Y("Agree/Disagree:Q", title="Is AI Regulation Needed?", scale=alt.Scale(domain=[0, 100])),
+        tooltip=[alt.Tooltip("Country:N", title="Country")], 
+        color=alt.Color("Sentiment:N",
+            scale = alt.Scale(domain=["Agree", "Disagree"], range=["#714141", "#d9d9de"])
+        )
+    ).transform_filter(
+        country_selector
+    ).interactive()
+
+    joint_chart = all_country_data | by_country2
+    st.write(joint_chart)
+
+    '''
+    From the chart above we can see that nearly every major world power agrees that AI imposes enough risk that it should be regulated. There is not one outlier nation that does not think that AI and Robotics are begning to play in increasingly dangerous or volatile role.
+    '''
 
 # -----------------------------------------------------------------------------
 # Conclusion
@@ -360,10 +397,10 @@ def render_conclusion_content():
     """
 
     '''
+    ---
     # Conclusion
     
     This concludes our exploration of the machine super intelligence! Don't worry, the machines already won.
-    
     '''
     
 # -----------------------------------------------------------------------------
@@ -381,6 +418,7 @@ def render_references():
     The works we referenced while working on this project are provided below.
 
     - McCarthy et al. _A Proposal for the Dartmouth Summer Research Project on Artificial Intelligence._ 1955.
+    - Muller, Vincent and Bostrom, Nick. Future Progress in Artificial Intelligence: A Survey of Expert Opinion. (2016).
     '''
 
 # -----------------------------------------------------------------------------
@@ -388,18 +426,24 @@ def render_references():
 # -----------------------------------------------------------------------------
 
 def main():
+
+    # NOTE: Naturally, the order of the invocations here
+    # defines the order in which our content is rendered
+    # in the application. For sanity's sake, please ensure
+    # that the order here matches the high-level order of
+    # each of the chapters as they are defined above.
+
     render_introduction_content()
 
     render_history_chapter()
 
-    # Chapter 1: TODO
-    render_terminator_chapter()
+    render_paradigm_chapter()
 
-    # Chapter 2: TODO
-    render_hal_chapter()
+    render_perceptions_chapter()
     
-    # Chapter 4: What would you choose?
-    render_user_choice()
+    render_prospects_chapter()
+
+    render_responses_chapter()
 
     render_conclusion_content()
 
