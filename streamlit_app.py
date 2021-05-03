7c0fa13eb6170ee14a56b574852355b7d48a72e5
WillBorom <70612659+WillBorom@users.noreply.github.com>
2021-05-03 12:11:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -134,7 +134,7 @@ def render_substrate_section():
     - Capacity: The human cranium imposes tight size limitations on our brains. A mechanical mind that implements a machine intelligence has no such size restrictions. QUESTION: How do we communicate the difference in volume between a human brain and a potential mechanical brain?
     '''
     from PIL import Image
-    image = Image.open('img/brain2.jpg')
+    image = Image.open('img/brain.png')
     st.image(image, caption='Brain', output_format='PNG')
 
 def render_paradigm_chapter():
